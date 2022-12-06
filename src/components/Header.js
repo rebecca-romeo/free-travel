d253ec7e456f50235a46b85a342cf23292eb8cf0
rebecca-romeo <rebecca.romeo@ryerson.ca>
2022-12-06 17:37:29 -0500

    updated header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,28 @@ const Header = () => {
   
   return (
     <header>
-        <h1>Free Travel</h1>
+      {/* Header wave effect made using https://www.app.websitebackgroundmaker.com */}
+<div class="wave">
+      <svg width="100%" height="200px" fill="none" version="1.1"
+      xmlns="http://www.w3.org/2000/svg">
+        <path 
+          fill="white" 
+          d="
+            M0 67
+            C 273,183
+              822,-41
+              1920.01,106 
+            V 359 
+            H 0 
+            V 67
+            Z">
+        </path>
+      </svg>
+    </div>
+    <div id="container">
+        <h1>FreeTravel</h1>
         <h2>explore the world from home</h2>
+    </div> 
     </header>
 
   )
